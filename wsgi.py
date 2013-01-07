ea7a1ceb61fa7a9833de1762c1df09967fcda526
Lynn Root <lroot@redhat.com>
2013-01-07 11:37:39 -0800

    fixed typo

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import static
 import os
 
-SITE_ROOT = os.path.dirname(os.path.realpath((__file__))
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 app = static.Cling(os.path.join(SITE_ROOT, '_site'))
