e904e84905bf525b3e77bf25df8f9fccfa84e9c4
florapdx <floraworleypdx@gmail.com>
2014-06-16 21:29:09 -0700

    rewrite meetup widget as module and add new chapters

--- www/_assets/js/meetup_widget.js
+++ www/_assets/js/meetup_widget.js
@@ -1,51 +1,94 @@
-///Asks Meetup.com for any public event listing from one of the PyLadies chapters upcoming in the next week
-//Localized for use by any chapter--just copy file over to your location site and sub your meetup key for "meetup_key" and meetup group id (indicated as data id on locations index.html page) for "chapter_ids" in addUpcomingMeetups function.
+/* Note: Updated to reflect JS Module pattern for encapsulation (v2)
+ * To call: PyladiesMeetupWidget.addUpcomingMeetups(chapterIds)
+ * Requests location meetups from Meetup.com coming up in the next month
+ * and uses data to construct html entries for each event that are appended
+ * to an '#upcomingMeetupsList' div in page's html.
+ * Can be given any number of chapterIds.
+ */
 
-//helper functions
-function create_urls(input) {
-        return input
-        .replace(/(ftp|http|https|file):\/\/[\S]+(\b|$)/gim, '"$&" target="_blank"')
-        .replace(/([^\/])(www[\S]+(\b|$))/gim, '"http://$2" target="_blank"');
-} //end url parsing helper
+var PyladiesMeetupWidget = (function() {
+  var key = '70704f53354b686770246f68494e2637';
 
-function convert_time_miliseconds_to_date(t) {
-        var d = new Date(t); // The 0 there is the key, which sets the date to the epoch
-        return String(d).slice(0, 11);
-} //end convert miliseconds_to_date
+  // Private methods //
 
-// functions
-function addUpcomingMeetups(chapter_ids) {
-   var meetup_key = '70704f53354b686770246f68494e2637';
+  _createUrls = function(url) {
+    // creates <a href> links from urls for event clickthroughs
+    return url
+      .replace(/(ftp|http|https|file):\/\/[\S]+(\b|$)/gim, '"$&" target="_blank"')
+      .replace(/([^\/])(www[\S]+(\b|$))/gim, '"http://$2" target="_blank"');
+  };
 
-   $.ajax( {
-       url: 'http://api.meetup.com/2/events.json?key='+meetup_key+'&group_id='+chapter_ids+'&fields=group_photo&time=0m,1m&status=upcoming&sign=true',
-       dataType: 'jsonp',
-       success: function(data) {
-          buildHtml(data);
-       }, //end success
-       error: function(data) {
-          handleError(data);
-       } //end error
-     }); //end .ajax()
-     return false;
-} //end addUpcomingMeetups function
+  _convertMilisecondsToDate = function(ms) {
+    // converts miliseconds since epoch to date format
+    var date = new Date(ms);
+    return String(date).slice(0, 11);
+  };
 
-function buildHtml(data) {
+  _makeAjaxRequest = function(ids) {
+    // fetch data
+    $.ajax({
+      url: 'http://api.meetup.com/2/events.json?key=' + key + '&group_id=' + ids +
+        '&fields=group_photo&time=0m,1m&status=upcoming&sign=true',
+      dataType: 'jsonp',
+      success: function(data) {
+        _buildHtml(data);
+      },
+      error: function(data) {
+        _handleError(data);
+      }
+    });
+  };
+
+  _getJSON = function(datum) {
+    // {_buildHtml helper}
+    // note: Not every meetup group event request returns a photo--not sure why, but
+    // noticed that neither Taiwan or Bangalore's logos returned, so perhaps there
+    // is some difference in data offered in parts of Asia?
+    var json = {
+      thumbLink: datum.group.group_photo ? datum.group.group_photo.thumb_link : "",
+      groupName: datum.group.name,
+      eventLink: _createUrls(datum.event_url),
+      eventName: datum.name,
+      eventDate: _convertMilisecondsToDate(datum.time)
+    };
+
+    return json;
+  };
+
+  _buildHtml = function(data) {
+    var html;
+    var datum;
+    var json;
+
+    // remove any old meetup list still attached to dom and append new $ul
     $('#upcomingMeetupsList ul').remove();
     $('#upcomingMeetupsList').append('<ul></ul>');
-    for (var key in data.results) {
-      url_link = create_urls(data.results[key].event_url);
-      event_date = convert_time_miliseconds_to_date(data.results[key].time);
-      var html = '<img class="meetup_group_icon" src="'+data.results[key].group.group_photo.thumb_link+'" height="80" width="80" />';
-      html += '<p class="meetup_listing_group">'+data.results[key].group.name+'</p>';
-      html += '<p class="meetup_listing_event_title"><a href='+url_link+'>'+data.results[key].name+'</a></p>';
-      html += '<p class="meetup_event_date">'+event_date+'</p>';
-      $('#upcomingMeetupsList').find('ul').append('<li>'+html+'</li>');
+
+    for (var i = 0; i < data.results.length; i++) {
+      datum = data.results[i];
+      json = _getJSON(datum);
+
+      // todo(fw): pull this out into a template
+      html = '<img class="meetup_group_icon" src="' + json.thumbLink + '" height="80" width="80" />';
+      html += '<p class="meetup_listing_group">' + json.groupName + '</p>';
+      html += '<p class="meetup_listing_event_title"><a href=' + json.eventLink + '>' + json.eventName + '</a></p>';
+      html += '<p class="meetup_event_date">' + json.eventDate + '</p>';
+
+      $('#upcomingMeetupsList ul').append('<li>' + html + '</li>');
     }
-}
+  };
 
-function handleError(data) {
+  _handleError = function(data) {
+    // remove any stale list attached to dom and print error message
     $('#upcomingMeetupsList ul').remove();
-    $('#upcomingMeetupsList').append('<ul></ul>');
-    $('#upcomingMeetupsList').find('ul').append('<li>Sorry, we are unable to reach Meetup.com at this time</li>');
-}
+    $('#upcomingMeetupsList')
+      .append('<ul><li>Sorry, we are unable to reach Meetup.com at this time</li></ul>');
+  };
+
+  return {
+    // returns public method
+    addUpcomingMeetups: function(chapterIds) {
+      _makeAjaxRequest(chapterIds);
+    }
+  };
+})();
