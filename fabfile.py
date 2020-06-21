d39abb4b7f13382c5f8d64cde2a6cde35270d347
lorenanicole <me@lorenamesa.com>
2020-06-21 16:06:23 -0500

    Update fabfile with new  fabric 2+ api, add  patchwork dependency

--- fabfile.py
+++ fabfile.py
@@ -1,29 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015 PyLadies, Lynn Root
+# Copyright (c) 2020 PyLadies, Lorena Mesa
 
-
-from fabric.api import env, lcd, local
-from fabric.contrib.project import rsync_project
-
-
-env.user = "u52703"
-env.keyfile_name = "~/.ssh/pyladies.com"
-env.hosts = "81.28.232.189"
-
-
-def prep_www_deploy():
-    with lcd("www"):  # locally change into www dir
-        local("mynt gen -f _site")
-
-
-def rsync_www():
-    with lcd("www"):
-        rsync_project(local_dir="_site/",
-                      remote_dir="www/",
-                      extra_opts="-ua")
+from fabric import Connection
+import patchwork.transfers
 
 
 def deploy_www():
-    prep_www_deploy()
-    rsync_www()
+    connect_kwargs = {'key_filename': ['~/.ssh/pyladies.com']}
+    with Connection(host='81.28.232.189', user='u52703', connect_kwargs=connect_kwargs) as connection:
+        # Prep WWW Deploy
+        connection.cwd('www')
+        connection.local('mynt gen -f _site')
+        # Rsync WWW
+        patchwork.transfers.rsync(connection, '_site/', 'www/', exclude='.git', rsync_opts='-ua')
