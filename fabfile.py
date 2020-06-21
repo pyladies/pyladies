081ddb22bbf9bb488e70c1a1c042009ed78afab9
Lorena Mesa <me@lorenamesa.com>
2020-06-21 18:37:53 -0500

    Update fabfile without keyfile
    Update fabfile without keyfile

--- fabfile.py
+++ fabfile.py
@@ -8,12 +8,11 @@ import patchwork.transfers
 def setup_context(ctx):
     ctx.host = '81.28.232.189'
     ctx.user = 'u52703'
-    ctx.connect_kwargs = {'key_filename': ['~/.ssh/pyladies.com']}
     return ctx
 
 @task
 def deploy(ctx):
     ctx = setup_context(ctx)
-    with Connection(host=ctx.host, user=ctx.user, connect_kwargs=ctx.connect_kwargs) as connection:
+    with Connection(host=ctx.host, user=ctx.user) as connection:
         # Rsync WWW
         patchwork.transfers.rsync(connection, 'www/_site/', 'www/', exclude='.git', rsync_opts='-ua')
